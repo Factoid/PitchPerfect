4ced6265cb9950fdbf6d38b3794e2eb14145c2b2
Adrian Cheater <adrian.cheater@gmail.com>
2021-02-06 13:25:51 -0600

    Cleaned up dir structure

--- make.sh
+++ make.sh
@@ -1,9 +1,10 @@
-gcc -Wall -Werror -fPIC -I./opus/include -I./opus -I./opus/celt $(pkg-config --cflags gstreamer-1.0 gstreamer-audio-1.0) -c -o gstopusvis.o gstopusvis.c
+mkdir -p ./build
+gcc -Wall -Werror -fPIC -I./lib/opus/include -I./lib/opus -I./lib/opus/celt $(pkg-config --cflags gstreamer-1.0 gstreamer-audio-1.0) -c -o build/gstopusvis.o src/gstopusvis.c
 if test $? -ne 0; then
     exit 1
 fi
 
-gcc -Wl,--whole-archive ./opus/.libs/libopus.a -Wl,--no-whole-archive -shared -o gstopusvis.so gstopusvis.o $(pkg-config --libs gstreamer-1.0 gstreamer-audio-1.0)
+gcc -Wl,--whole-archive ./lib/opus/.libs/libopus.a -Wl,--no-whole-archive -shared -o build/gstopusvis.so build/gstopusvis.o $(pkg-config --libs gstreamer-1.0 gstreamer-audio-1.0)
 if test $? -ne 0; then
     exit 1
 fi
